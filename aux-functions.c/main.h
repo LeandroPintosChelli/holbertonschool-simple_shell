7253b59bb0fb99b0007fa2d1d387fe0e8cdeb652
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-29 07:46:23 -0700

    Aux functions

--- aux-functions.c/main.h
+++ aux-functions.c/main.h
@@ -7,6 +7,6 @@ int _strlen(char *s);
 void _puts(char *str);
 char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
-char *_strcpy(char *dest, char *src);
+char *_strdup(char *str);
 int _putchar(char c);
 #endif
