6d638a138183568d10cac87f7e7c663607e49f7c
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-29 13:59:02 -0700

    Simple shell

--- more_aux_functions.c
+++ more_aux_functions.c
@@ -25,3 +25,34 @@ char *_which(char *str)
 	}
 	return (NULL);
 }
+
+/**
+* _getenv - function to do getenv
+* @name: pointer
+* Return: NULL
+*/
+char *_getenv(const char *name)
+{
+	char *path;
+	int i, j;
+	int status;
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+	status = 1;
+	for (j = 0; environ[i][j] != '='; j++)
+	{
+		if (name[j] != environ[i][j])
+		{
+			status = 0;
+			break;
+		}
+	}
+	if (status)
+	{
+		path = _strdup(&environ[i][j + 1]);
+		return (path);
+	}
+	}
+	return (NULL);
+}
