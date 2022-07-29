eedff39af8b65f6b84b70723ef5aeeae43629bb0
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-29 09:02:46 -0700

    Simple shell

---
+++ simple-shell.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	pid_t child;
+	char *command[16], *tok, *lineptr = NULL;
+	size_t i, n;
+	int status;
+  
+	while (1)
+	{
+	printf("$ ");
+	if (getline(&lineptr, &n, stdin) == -1)
+	break;
+	tok = strtok(lineptr, " \t\n\r");
+	for (i = 0; i < 16 && tok != NULL; i++)
+	{
+		command[i] = tok;
+		tok = strtok(NULL, " \t\n\r");
+	}
+	command[i] = NULL;
+	if (fork() == 0)
+	{
+		if (execve(command[0], command, NULL) == -1)
+		{
+			perror("execve");
+			free(tok);
+			free(lineptr);
+			exit(0);
+		}
+	}
+		if (child > 0)
+		wait(&status);
+	}
+	printf("\n");
+	free(lineptr);
+	exit(status);
+}
