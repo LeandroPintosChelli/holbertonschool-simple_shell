15c34d5467c0ea4dc1f9bb63725764887b7cc4da
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-29 10:01:56 -0700

    Str-concat

--- aux-functions/aux-functions.c
+++ aux-functions/aux-functions.c
@@ -17,21 +17,35 @@ int _strlen(char *s)
 }
 
 /**
- *_strcat - string cat
- * @dest: a
- * @src: copies the string pointed
- * Return: Always 0.
- */
-char *_strcat(char *dest, char *src)
+* str_concat - check code
+* @s1: a
+* @s2: b
+* Return: Always 0
+*/
+char *str_concat(char *s1, char *s2)
 {
-	size_t dest_len = _strlen(dest);
-	size_t i;
+	int i = 0;
+	char *c;
+	int a, b;
 
-	for (i = 0 ; src[i] != '\0'; i++)
-	dest[dest_len + i] = src[i];
-	dest[dest_len + i] = '\0';
-
-	return (dest);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	a = strlen(s1);
+	b = strlen(s2);
+	c = (char *)malloc(sizeof(char) * ((a + b) + 1));
+	if (c == NULL)
+		return (NULL);
+	for (i = 0; i < a; i++)
+	{
+		c[i] = s1[i];
+	}
+	for (i = 0; i < b; i++)
+	{
+		c[i + a] = s2[i];
+	}
+	return (c);
 }
 
 /**
