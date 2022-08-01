9ea3c27f62443fe6c353927995e652fa9c6bff8b
LeandroPintosChelli <leandropintos162@gmail.com>
2022-08-01 07:10:14 -0700

    Simple Shell

--- simple_shell.c
+++ simple_shell.c
@@ -15,13 +15,12 @@ int main(__attribute__((unused)) int ac, char **av, char **env)
 
 	while (1)
 	{
-/*		printf("#blas$ ");*/
 		if (getline(&input, &n, stdin) == -1)
 		{
 			free(input);
 			break;
 		}
-		if (strcmp(input, "\n") == 0)
+		if (_strcmp(input, "\n") == 0)
 			continue;
 		input = strtok(input, "\n");
 		if (input && stat(input, &file) == 0)
@@ -32,7 +31,6 @@ int main(__attribute__((unused)) int ac, char **av, char **env)
 		else
 		{
 			perror("");
-/*			printf("%s: No such file or directory\n", av[0]);*/
 			continue;
 		}
 		wait(&status);
