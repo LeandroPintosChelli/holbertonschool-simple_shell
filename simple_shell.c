db5e2d1be5aca5704fcac27c3cf7e936e3addb2a
LeandroPintosChelli <leandropintos162@gmail.com>
2022-08-02 13:53:57 -0700

    Simple Shell

--- simple_shell.c
+++ simple_shell.c
@@ -11,38 +11,37 @@ int main(__attribute__((unused)) int ac, char **env)
 	char *input = NULL, *buf[1024], *tok;
 	size_t i, n = 0;
 	int status = 0;
+	int child;
 	struct stat file;
 
-while (1)
-{
-	if (getline(&input, &n, stdin) == -1)
-		break;
-	tok = strtok(input, " \t\n\r");
-	for (i = 0; i < 1024 && tok != NULL; i++)
+	while (1)
 	{
-		buf[i] = tok;
-		tok = strtok(NULL, " \t\n\r");
-	}
-	buf[i] = NULL;
-	if (_strcmp(input, "env") == 0)
-	{
-		_env(env);
-		continue;
-	}
-	if (stat(input, &file) == 0)
-	{
-		if (fork() == 0)
+		if (getline(&input, &n, stdin) == -1)
+			break;
+		tok = strtok(input, " \t\n\r");
+		for (i = 0; i < 1024 && tok != NULL; i++)
+		{
+			buf[i] = tok;
+			tok = strtok(NULL, " \t\n\r");
+		}
+		buf[i] = NULL;
+		if (!buf[0])
+		{
+			free(buf[0]);
+			free(input);
+			return (0);
+		}
+		child = fork();
+		if (child == 0)
 		{
 			if (execve(buf[0], buf, env) == -1)
 			{
+				perror("");
 				return (0);
 			}
 		}
 		else
 			wait(&status);
-	}
-	else
-		perror("");
 	}
 	free(input);
 	return (0);
