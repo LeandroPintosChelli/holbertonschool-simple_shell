a1f885315d2ec619d84545a645a186f0cd6194a2
Blas <blashernandez98@gmail.com>
2022-08-03 12:21:29 -0700

    prueba-checker

--- simple_shell.c
+++ simple_shell.c
@@ -1,65 +1,55 @@
 #include "main.h"
 /**
 * main - simple shell
-* @ac: unused
-* @av: void
-* @env: unused
-* Return: 0
+* @ac: Number of arguments passed to main.
+* @av: Array of string arguments.
+* @env: Enviroment variables.
+*
+* Return: Always 0.
 */
+
 int main(__attribute__((unused)) int ac, char **av, char **env)
 {
-	char *input = NULL, *buf[1024], *tok;
-	size_t e = 0, i = 0, n = 0;
+	char *input = NULL, *buf[1024], *tok, *path = NULL;
+	size_t i, n = 0;
 	int status = 0;
-	int child;
+	struct stat file;
+	void (*builtin)(char **, char *);
 
-	(void) av;
 	while (1)
 	{
+		if (isatty(STDIN_FILENO) == 1)
+			write(1, "$ ", 2);
 		if (getline(&input, &n, stdin) == -1)
 			break;
 		tok = strtok(input, " \t\n\r");
-		if (!tok)
-			break;
-		if (_strcmp(tok, "exit") == 0)
-		{
-			free(input);
-			return (i);
-		}
+		if (tok == NULL)
+			continue;
 		for (i = 0; i < 1024 && tok != NULL; i++)
 		{
 			buf[i] = tok;
 			tok = strtok(NULL, " \t\n\r");
 		}
 		buf[i] = NULL;
-		if (!buf[0])
+		builtin = check_builtin(buf[0]);
+		if (builtin != NULL)
 		{
-			free(buf[0]);
-			free(input);
-			return (0);
-		}
-		if (_strcmp(input, "env") == 0)
-		{
-			for (e = 0; env[e] != NULL; e++)
-			{
-				printf("%s\n", env[e]);
-			}
+			(*builtin)(env, input);
 			continue;
 		}
-		if (pathver(buf[0]) == -1)
-			buf[0] = _which(buf[0]);
-		child = fork();
-		if (child == 0)
+		path = _which(buf[0]);
+		if (path == NULL)
 		{
-			if (execve(buf[0], buf, env) == -1)
-			{
-				perror("");
-				return (0);
-			}
+			perror("");
+			continue;
 		}
+		buf[0] = path;
+		if (fork() == 0)
+			execve(buf[0], buf, env);
 		else
 			wait(&status);
-	free(buf[0]);
+		if (path && path != input)
+			free(path);
 	}
 	free(input);
 	return (0);
