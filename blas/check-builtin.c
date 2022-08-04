2c8ff3779e0a8e2c467f9248b67a29308d0379ce
Blas <blashernandez98@gmail.com>
2022-08-04 10:32:16 -0700

    task4_wokrs

---
+++ blas/check-builtin.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * check_builtin - Checks for builtin function.
+ * @str: String input.
+ * 
+ * Return: Pointer to built-in function or NULL if not found.
+ */
+
+void (*check_builtin(char *str))(char **, char *)
+{
+	built_t arr[] = {
+		{"blas", _env},
+		{"exit", _salir}	
+	};
+	int i;
+
+	if (!str)
+		return (NULL);
+	for (i = 0; i < 2; i++)
+	{
+		if (_strcmp(str, arr[i].name) == 0)
+			return (arr[i].f);
+	}
+	return (NULL);
+}
--- blas/more_aux_functions.c
+++ blas/more_aux_functions.c
@@ -48,13 +48,18 @@ int _putchar(char c)
 * @env: pointer to a char
 * Return: void
 */
-void p_error(char *cmd, char *program, int *status)
+void _env(__attribute__((unused)) char **env, __attribute__((unused)) char *freeme)
 {
-	write(2, program, _strlen(program));
-	write(2, ": 1: ", 5);
-	write(2, cmd, _strlen(cmd));
-	write(2, ": not found\n", 12);
-	*status = 127;
+	/*size_t len;
+
+	for (; *env;)
+	{
+		len = strlen(*env);
+		write(1, *env, len);
+		_putchar('\n');
+		*env = *env + len;
+	}*/
+	printf("envioo\n");
 }
 
 /**
@@ -65,10 +70,7 @@ void p_error(char *cmd, char *program, int *status)
 void _salir(__attribute__((unused)) char **env, char *freeme)
 {
 	free(freeme);
-	if (status == 0)
-		exit(0);
-	else
-		exit(2);
+	exit(2);
 }
 
 /**
